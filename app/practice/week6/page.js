cd4f009c099efd013e93e15c2631dbebf01a0397
XMmenghan <96541054+XMmenghan@users.noreply.github.com>
2024-10-16 15:42:50 -0600

    week6 update

---
+++ app/practice/week6/page.js
@@ -0,0 +1,49 @@
+"use client";
+import { useState } from "react";
+import dogsJson from "./dogs.json"; // dogsJson is an array of objects
+import Image from "next/image";
+
+export default function Page() {
+  const [selectedDogId, setSelectedDogId] = useState(-1);
+  let dogs = [...dogsJson]; // shallow copy of dogsJson
+  dogs.sort((a, b) => a.name.localeCompare(b.name)); // sort by name
+
+  dogs = dogs.filter((dog) => dog.name.length > 2); // filter by name length
+
+  const handleClick = (id) => {
+    if (selectedDogId === id) {
+      setSelectedDogId(-1);
+      return;
+    }
+    setSelectedDogId(id);
+  };
+
+  return (
+    <main className="p-2">
+      <h1 className="text-2xl font-semibold">Week 6</h1>
+      <p>Current id: {selectedDogId}</p>
+      <ul>
+        {dogs.map((dog) => (
+          <li
+            key={dog.name}
+            className={`py-2 m-2 ${
+              dog.id === selectedDogId ? "bg-gray-200" : "bg-gray-100"
+            }`}
+            onClick={() => handleClick(dog.id)}
+          >
+            <h2 className="text-lg font-semibold">{dog.name}</h2>
+            <p className="text-sm">{dog.description}</p>
+            <p>
+              <Image
+                src={dog.imageUrl}
+                alt={dog.name}
+                width={360}
+                height={180}
+              />
+            </p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,92 +1,53 @@
+"use client";
+//Remove all item variables (const item1 etc.) from the ItemList component.
+//Import the useState hook from React, the Item component, and the items from the JSON file.
 import Item from "./item";
+import { useState } from "react";
+import itemJson from "./item.json";
 
 export default function ItemList() {
-    const item1 = {
-        name: "milk, 4 L 🥛",
-        quantity: 1,
-        category: "dairy",
-      };
-       
-      const item2 = {
-        name: "bread 🍞",
-        quantity: 2,
-        category: "bakery",
-      };
-       
-      const item3 = {
-        name: "eggs, dozen 🥚",
-        quantity: 2,
-        category: "dairy",
-      };
-       
-      const item4 = {
-        name: "bananas 🍌",
-        quantity: 6,
-        category: "produce",
-      };
-       
-      const item5 = {
-        name: "broccoli 🥦",
-        quantity: 3,
-        category: "produce",
-      };
-       
-      const item6 = {
-        name: "chicken breasts, 1 kg 🍗",
-        quantity: 1,
-        category: "meat",
-      };
-       
-      const item7 = {
-        name: "pasta sauce 🍝",
-        quantity: 3,
-        category: "canned goods",
-      };
-       
-      const item8 = {
-        name: "spaghetti, 454 g 🍝",
-        quantity: 2,
-        category: "dry goods",
-      };
-       
-      const item9 = {
-        name: "toilet paper, 12 pack 🧻",
-        quantity: 1,
-        category: "household",
-      };
-       
-      const item10 = {
-        name: "paper towels, 6 pack",
-        quantity: 1,
-        category: "household",
-      };
-       
-      const item11 = {
-        name: "dish soap 🍽️",
-        quantity: 1,
-        category: "household",
-      };
-       
-      const item12 = {
-        name: "hand soap 🧼",
-        quantity: 4,
-        category: "household",
-      };
-    
-    return(
-        <div>
-          <Item{...item1} />
-          <Item{...item2} />
-          <Item{...item3} />
-          <Item{...item4} />
-          <Item{...item5} />
-          <Item{...item6} />
-          <Item{...item7} />
-          <Item{...item8} />
-          <Item{...item9} />
-          <Item{...item10} />
-          <Item{...item11} />
-          <Item{...item12} />
+  const [sortBy, setSortBy] = useState("name");
+  let item = [...itemJson];
+  if (sortBy === "name") {
+    item.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortBy === "category") {
+    item.sort((a, b) => a.category.localeCompare(b.category));
+  }
+
+  return (
+    <div>
+      <div className="sort-buttons">
+        <button
+          onClick={() => setSortBy("name")}
+          className={
+            sortBy === "name"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-black"
+          }
+        >
+          Sort by Name
+        </button>
+        <button
+          onClick={() => setSortBy("category")}
+          className={
+            sortBy === "category"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-black"
+          }
+        >
+          Sort by Category
+        </button>
+      </div>
+      <div>
+        {item.map((item) => (
+          <Item
+            key={item.name}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+          />
+        ))}
+      </div>
     </div>
-    )
+  );
 }
