d28cafc056c6d3658b4c0aab7af9062e79d3790a
XMmenghan <96541054+XMmenghan@users.noreply.github.com>
2024-11-26 23:11:11 -0700

    RMS Project Phase1

--- app/week-2/page.js
+++
@@ -1,9 +0,0 @@
-import Heading from './student-info.js';
-export default function Page(){
-  return (
-    <main>
-    <Heading/>
-      <h1>Shopping List</h1>
-    </main>
-  );
-}
--- app/week-5/page.js
+++
@@ -1,9 +0,0 @@
-import NewItem from "./new-item";
-
-export default function Page() {
-    return (
-        <main className="flex justify-center bg-black min-h-screen" >
-            <NewItem />
-        </main>
-    );
-}
